2e5ae1cb393c98bdf02774025e0ca801cf91474b
Noé Chouteau <osolarix@gmail.com>
2023-04-01 21:33:51 +0200

    Animation et regroupement par date

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,96 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+let allDivs = document.querySelectorAll('.derniereListeContainer');
+let done = false;
+
+allDivs.forEach(function (div) {
+    div.children[0].children[1].addEventListener('click', function (e) {
+        if (div.children[1].style.display == 'none') {
+            allDivs.forEach(function (div2) {
+                if(div != div2){
+                    if(done == true){
+                        div2.style.position = 'relative';
+                        div.children[1].style.display = 'flex';
+                        div2.style.top = div.offsetHeight+ "px"; 
+                        div.children[1].style.display = 'none';
+                    }
+                }else{
+                    done = true;
+                }
+            });
+            setTimeout(function () {
+                div.children[1].style.display = 'flex';
+                allDivs.forEach(function (div2) {
+                    if( div != div2){
+                        div2.style.position = 'static';
+                        div2.style.top = "0";
+                    }
+                });
+                setTimeout(function () {
+                    div.children[1].style.opacity = 1;
+                }, 20);
+            }, 500);
+            div.children[0].children[1].style.background = 'white';
+            div.children[0].children[1].style.color = 'black';
+            done = false;
+        } else if (!div.children[1].style.display) {
+            allDivs.forEach(function (div2) {
+                if( div != div2){
+                    if(done == true){
+                        div2.style.position = 'relative';
+                        div.children[1].style.display = 'flex';
+                        div2.style.top = div.offsetHeight+ "px"; 
+                        div.children[1].style.display = 'none';
+                    }
+                }else{
+                    done = true;
+                }
+            });
+            setTimeout(function () {
+                div.children[1].style.display = 'flex';
+                allDivs.forEach(function (div2) {
+                    if( div != div2){
+                        div2.style.position = 'static';
+                        div2.style.top = "0";
+                    }
+                });
+                setTimeout(function () {
+                    div.children[1].style.opacity = 1;
+                }, 20);
+            }, 500);
+            div.children[0].children[1].style.background = 'white';
+            div.children[0].children[1].style.color = 'black';
+            done = false;
+        }
+        else {
+            div.children[1].style.opacity = 0;
+            setTimeout(function () {
+                allDivs.forEach(function (div2) {
+                    if( div != div2){
+                        if(done == true){
+                            div2.style.position = 'relative';
+
+                            div2.style.top = "-"+(div.offsetHeight - 78)+ "px"; 
+                        }
+                    }else{
+                        done = true;
+                    }
+                });
+                setTimeout(function () {
+                    div.children[1].style.display = 'none';
+                    allDivs.forEach(function (div2) {
+                        if( div != div2){
+                            div2.style.position = 'static';
+                            div2.style.top = "0";
+                        }
+                        done = false;
+                    });
+                }, 500);
+            }, 300);
+            div.children[0].children[1].style.background = 'black';
+            div.children[0].children[1].style.color = 'white';
+        }
+    });
+});
